9a216730ff212a7a5e1f71bf65e9b596aabd9626
Nakanishi-wsl <ponzxp@gmail.com>
2024-02-18 18:13:13 +0900

    complete

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 const prisma = new PrismaClient();
 
@@ -11,9 +13,62 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello~\n');
 })
 
+// ユーザー取得
 app.get('/users', async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   res.json(users);
+})
+
+// ユーザー作成
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const data = {
+    name: name,
+    email: email
+  }
+
+  try {
+    const newUser = await prisma.user.create({
+      data: data
+    })
+    res.json(newUser);
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
+})
+
+// ユーザー更新
+app.put('/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+
+  const data = {
+    name: name,
+    email: email
+  }
+
+  try {
+    const updateUser = await prisma.user.update({
+      where: { id: parseInt(id) },
+      data: data
+    })
+    res.json(updateUser);
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
+})
+
+// ユーザー削除
+app.delete('/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await prisma.user.delete({
+      where: { id: parseInt(id) }
+    })
+    res.json(deleteUser);
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
 })
 
 app.listen(port, () => {
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,83 @@
-import './App.css'
+import axios from 'axios';
+import { useState } from 'react';
+import './App.css';
+
+type User = {
+  id: number,
+  name: string,
+  email: string
+}
 
 function App() {
 
-  /** Memoデータを取得 */
-  const readMemoData = () => {
-    //Read
+  const [users, setUsers] = useState<User[]>([]);
+
+  /** Userデータを取得 */
+  const readUserData = () => {
+    axios.get("http://localhost:3001/users")
+      .then(res => setUsers(res.data))
+      .catch(e => console.error(e))
+  }
+
+  // 登録するデータ
+  const data = {
+    name: "Tana",
+    email: "sample@gmail.com",
+  }
+
+  /** Userデータを作成 */
+  const createUserData = () => {
+    axios.post("http://localhost:3001/users", data)
+      .then(res => alert("データ作成完了しました!"))
+      .catch(e => console.error(e))
+  }
+
+  // 更新するユーザー
+  const userNum = 3;
+
+  /** Userデータを更新 */
+  const updateUserData = () => {
+    axios.put(`http://localhost:3001/users/${userNum}`, data)
+      .then(res => alert("データ更新完了しました!"))
+      .catch(e => console.error(e))
+  }
+
+  /** Userデータを削除 */
+  const deleteUserData = () => {
+    axios.delete(`http://localhost:3001/users/${userNum}`)
+      .then(res => alert("データ削除完了しました!"))
+      .catch(e => console.error(e))
   }
 
   return (
     <div>
-      <p>Web Page</p>
-      <button onClick={readMemoData} >データ取得</button>
+      <h1>Sample Page</h1>
+      <button onClick={readUserData} >データ取得</button>
+      <button onClick={createUserData} >データ作成</button>
+      <button onClick={updateUserData} >データ更新</button>
+      <button onClick={deleteUserData} >データ削除</button>
+
+      <div>
+        <h2>データ表示</h2>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>名前</th>
+              <th>メール</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users && users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
